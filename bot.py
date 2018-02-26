9d3a59eba877f60f56bb326b7fa24a7a96e0bd96
moreabsence <moreabsence@gmail.com>
2018-02-26 19:24:07 +0300

    Добавил боту функцию калькулятора

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 from ephem import *
 
+from collections import Counter
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 def greet_user(bot, update):
@@ -18,31 +20,79 @@ def update(bot, update):
     update.message.reply_text(update.to_dict())
 
 def planet(bot, update):
-    planet_name = (update.message.text) 
+    planet_name = (update.message.text)
+    planet_name = planet_name.replace('/planet ', '') 
+    print(planet_name)
     planets = {
-'Mercury' : Mercury,
-'Moon': Moon,
-'Venus': Venus,
-'Mars' : Mars, 
-'Jupiter' : Jupiter, 
-'Saturn' : Saturn, 
-'Uranus' : Uranus, 
-'Neptune' : Neptune, 
-'Pluto' : Pluto, 
-'Sun' : Sun, 
-'Moon' : Moon
-}
+    'Mercury' : Mercury,
+    'Moon': Moon,
+    'Venus': Venus,
+    'Mars' : Mars, 
+    'Jupiter' : Jupiter, 
+    'Saturn' : Saturn, 
+    'Uranus' : Uranus, 
+    'Neptune' : Neptune, 
+    'Pluto' : Pluto, 
+    'Sun' : Sun, 
+    'Moon' : Moon
+    }
     m = planets.get(planet_name) 
     location = constellation(m('2018/2/23'))
     if planet_name in planets:
             update.message.reply_text(location)
+    else:
+            update.message.reply_text(planet_name)
+
+def wordcount(bot, update):
+    input_text = update.message.text
+    input_text = input_text.replace('/wordcount', '')
+    words = input_text.split()
+    bad_words = ['.','?','!','&','*']
+    for x in bad_words:
+        if x in words:
+            words.remove(x)
+    words_sum = len(words)
+    if words_sum == 0:
+        update.message.reply_text('Вы забыли ввести слова. Бывает.')
+    else:
+        update.message.reply_text('{} слова'.format(words_sum))
+
+def calculate(bot, update):
+    calc_string = update.message.text
+    if calc_string.endswith('='):
+        calc_string = calc_string.replace('=', '')
+        if '-' in calc_string:
+            numbers = calc_string.split('-')
+            result = int(numbers[0]) - int(numbers[1])
+            update.message.reply_text(result)
+        if '+' in calc_string:
+            numbers = calc_string.split('+')
+            result = int(numbers[0]) + int(numbers[1])
+            update.message.reply_text(result)
+        if '/' in calc_string:
+            numbers = calc_string.split('/')
+            result = int(numbers[0]) / int(numbers[1])
+            update.message.reply_text(result)
+        if '*' in calc_string:
+            numbers = calc_string.split('*')
+            result = int(numbers[0]) * int(numbers[1])
+            update.message.reply_text(result)
+        if '**' in calc_string:
+            numbers = calc_string.split('**')
+            result = int(numbers[0]) ** int(numbers[1])
+            update.message.reply_text(result)
+    else:
+        update.message.reply_text('Сама ты {}'.format(calc_string))
+
 
 def main():
     updater = Updater("504744759:AAFLDudnlMRy7yyp196gpyAqQtbdntWEwkw")
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("update", update))
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, planet))
+    dp.add_handler(CommandHandler('planet', planet))
+    dp.add_handler(CommandHandler('wordcount', wordcount))
+    dp.add_handler(MessageHandler(Filters.text, calculate))
     updater.start_polling()
     updater.idle()
 
